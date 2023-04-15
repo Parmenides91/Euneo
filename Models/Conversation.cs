3c7adf319a68fbd4cf1a9a72051f26818f51538c
Roberto Benéitez Vaquero <robertobv@usal.es>
2023-04-15 02:39:30 +0200

    Add project files.

---
+++ Models/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Euneo.Models
+{
+    public class Conversation
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime ImportedDate { get; set; }
+    }
+}
